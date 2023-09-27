3a1cfa0d69f598e487b6172eb55967e8cbc246b7
csuftitan <csuftitan@MacBook-Air-179.local>
2023-09-27 00:13:49 -0700

    Add checks to determine whether inputFile and outputFile were opened successfully

--- main.cpp
+++ main.cpp
@@ -9,4 +9,20 @@ int main() {
 
     // Creating an ofstream object called outputFile and opening file2.txt for writing to file2.txt
     std::ofstream outputFile("file2.txt");
+
+    // Checking if inputFile was opened successfully
+    if (!inputFile.is_open()) {
+        // Priting out error message if inputFile could not be opened
+        std::cerr << "Error opening input file." << std::endl;
+        // Returning 1 to main to signify unsuccessful run of the program
+        return 1;
+    }
+
+    // Checking if outputFile was opened successfully
+    if (!outputFile.is_open()) {
+        // Printing out error message if outputFile could not be opened
+        std::cerr << "Error opening output file." << std::endl;
+        // Returning 1 to signify unsuccessful run of the program
+        return 1;
+    }
 }
